a5ad4dc3969fbd816cc2390e5fa12339b22f2f65
jaimemonroy <monroywg@gmail.com>
2025-11-05 13:34:29 -0400

    correct 4

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER'			, '127.0.0.1');
 define('DB_PORT'			, '5432');
 define('DB_USER'			, 'ketadmin');
-define('DB_PASS'			, 'Unimet_791906_5');
+define('DB_PASS'			, 'LondonTown');
 define('DB_NAME'			, 'ketdb');
 
 ?>
