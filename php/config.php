8be3be261a56ab6629f9410864d1f7ecd8cf896c
jaimemonroy <monroywg@gmail.com>
2025-11-05 13:19:08 -0400

    correct:RESTAURAR CONFIG 2

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER'			, 'ketelectropartes.com');
+define('DB_SERVER'			, 'localhost');
 define('DB_PORT'			, '5432');
 define('DB_USER'			, 'ketadmin');
 define('DB_PASS'			, 'Unimet_791906_5');
