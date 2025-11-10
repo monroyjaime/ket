c4ea176e0277d5f06b640d7506f87f4ce8946f5a
jaimemonroy <monroywg@gmail.com>
2025-11-10 11:31:41 -0400

    corr: ruta correcta respeto a js/presucuesto.js en el ajax

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -773,7 +773,7 @@ function guardarPresupuesto() {
 
         $.ajax({
             //url: "../../php/guardarPresupuesto.php?cache=" + Date.now(),  // EVITA CACHÉ
-            url: "../../php/guardar_presupuesto_final.php",
+            url: "admin/php/guardar_presupuesto_final.php",
             type: "POST",
             data: paramJSON,
             contentType: "application/json; charset=utf-8",
