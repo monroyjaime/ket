664f335e61090560c35bb39bd32ff9dbeee22f12
jaimemonroy <monroywg@gmail.com>
2025-11-14 19:26:37 -0400

    feat: 2da. version de fotos de presupuestos step 2

--- admin/presupuestos/presupuestoImages.php
+++ admin/presupuestos/presupuestoImages.php
@@ -1,5 +1,10 @@
 <?php
 //require_once("app/php/db.php");
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once("https://ketelectropartes.com/php/dbcat.php");
 
 
