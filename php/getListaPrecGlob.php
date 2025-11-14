b7e347381b34fa59841daef3f3d2cdd19e3e6eec
jaimemonroy <monroywg@gmail.com>
2025-11-14 10:16:06 -0400

    corr: debug 5

--- php/getListaPrecGlob.php
+++ php/getListaPrecGlob.php
@@ -26,8 +26,8 @@ if(($dptoID >0 && $lineaId==0) || ($lineaId >0 && $dptoId ==0))
         $query .= " ORDER BY a.dpto_id, a.code";
     else
         $query .= " ORDER BY a.code";
-    echo "query: ".$query."\n";
-    exit;
+    //echo "query: ".$query."\n";
+    //exit;
     $consult = $db->consultas($query);
     foreach ($consult as $value){
         $objRtn = new stdClass();
