8278e4ff0003b9d949d572aadc8faff87eb96347
jaimemonroy <monroywg@gmail.com>
2026-03-17 09:43:14 -0400

    fix: psw

--- generar_catalogo_3x7.py
+++ generar_catalogo_3x7.py
@@ -264,7 +264,7 @@ def main():
         'port': 5432,
         'database': 'ketdb',
         'user': 'ketadmin',
-        'password': 'ColocarPasswordAqui'
+        'password': 'LondonTown'
     }
     
     base_url = "https://ketelectropartes.com/catalogo/indexDpto3x7.php"
