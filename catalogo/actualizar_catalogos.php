c54008d354009871430667fc1cbb925b578d53a6
jaimemonroy <monroywg@gmail.com>
2026-03-24 13:28:28 -0400

    fix: 7.- cambiar aspecto de actualizar_catalogos.php para que se asemeje al estilo de la web

--- catalogo/actualizar_catalogos.php
+++ catalogo/actualizar_catalogos.php
@@ -43,7 +43,7 @@ foreach ($departamentos as $d) {
         /* Barra superior estilo página principal */
         .top-bar {
             background-color: #DDD;
-            padding: 10px 10px;
+            padding: 5px 10px;
         }
         
         .top-bar .row {
