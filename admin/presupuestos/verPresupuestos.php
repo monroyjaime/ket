4fd0a67182cd7c3b11cdc56d5a0a2e2db185c04f
jaimemonroy <monroywg@gmail.com>
2026-03-30 11:22:47 -0400

    fix: 8.- ver Presupuestos.php tambien ordenar por presupuesto_num los resultados de las busquedas

--- admin/presupuestos/verPresupuestos.php
+++ admin/presupuestos/verPresupuestos.php
@@ -401,49 +401,11 @@ try {
         // Reconstruir el select en el orden correcto
         rebuildSelect();
         
-        // Inicializar Tom Select con ordenamiento personalizado
+        // Inicializar Tom Select (versión simplificada - sin score)
         presupuestosTomSel = new TomSelect("#presupuestos-tom-sel", {
             searchField: ["text"],
             placeholder: "Buscar presupuesto...",
             maxOptions: 100,
-            // Ordenar resultados de búsqueda por presupuesto_num DESC
-            score: function(search) {
-                var scores = [];
-                var self = this;
-                var searchTerm = search.toLowerCase();
-                
-                this.options.forEach(function(option, key) {
-                    var text = option.text.toLowerCase();
-                    var score = 0;
-                    
-                    if (text === searchTerm) {
-                        score = 100;
-                    } else if (text.indexOf(searchTerm) === 0) {
-                        score = 90;
-                    } else if (text.indexOf(searchTerm) > -1) {
-                        score = 80;
-                    }
-                    
-                    if (score > 0) {
-                        var presupuestoNum = parseInt(option.attributes['data-presupuesto-num']?.value || 0);
-                        scores.push({
-                            key: key,
-                            score: score,
-                            presupuestoNum: presupuestoNum
-                        });
-                    }
-                });
-                
-                // Ordenar por puntuación (relevancia) y luego por número de presupuesto DESC
-                scores.sort(function(a, b) {
-                    if (a.score !== b.score) {
-                        return b.score - a.score;
-                    }
-                    return b.presupuestoNum - a.presupuestoNum;
-                });
-                
-                return scores.map(function(s) { return s.key; });
-            },
             onChange: function(value) {
                 if (value) {
                     presupuestoActualId = value;
